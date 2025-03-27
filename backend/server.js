5e9c8bbcb01c5115ffc5e166718006fc52190be8
Manish Kumar <manishprajapati.cs1@gmail.com>
2025-03-27 10:27:33 +0530

    update login and signup pages

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,16 @@ import cookieParser from "cookie-parser";
 
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
 
+app.use(cors({
+    origin: 'http://localhost:3000', // Allow only your frontend origin
+    methods: ['GET', 'POST'], // Allowed methods
+    credentials: true, // Allow cookies or other credentials to be sent
+  }));
 app.use(express.json());
 app.use(cookieParser());
 
