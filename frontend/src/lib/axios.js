86cf915fe8ee7bf14236a6098801f528b1608fb1
AKASH KUMAR <akashkumarcs23@gmail.com>
2025-04-24 10:26:37 +0530

    do some changes

--- frontend/src/lib/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_MODE === "development" ? "http://localhost:5000/api" : "/api",
-    withCredentials: true,
-});
